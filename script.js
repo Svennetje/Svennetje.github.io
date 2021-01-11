e21302f33d7b629831bd343f4ca3eafdf63266f6
Sven Hansen <svenroermond@hotmail.com>
2021-01-12 00:35:47 +0100

    Added navbar responsivity & changed design landing section
    Added navbar responsivity for mobile toggler animation & changed text and styling for landing section

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 $(document).ready(function(){
 
     // Add smooth scrolling to all links
@@ -12,32 +33,45 @@ $(document).ready(function(){
             // Store hash
             var hash = this.hash;
 
-            // Let the navbar collapse in first (for 325ms) before scrolling to selected navbar-link
-            setTimeout(function() {
-                
-                // Using jQuery's animate() method to add smooth page scroll
+            // If device is a mobile
+            if (isMobile.any()) {
+
+                // Let the navbar collapse in first (for 325ms) before scrolling to selected navbar-link
+                setTimeout(function() {
+                    
+                    // Using jQuery's animate() method to add smooth page scroll
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800, function(){
+                        // Add hash (#) to URL when done scrolling (default click behavior)
+                        window.location.hash = hash;
+                    });
+                }, 325)  
+            } 
+
+            // If device is not a mobile
+            else {
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
                 }, 800, function(){
                     // Add hash (#) to URL when done scrolling (default click behavior)
                     window.location.hash = hash;
                 });
-            }, 325)            
+            }         
         }
     });
+});
+
+// if(isMobile()) {
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$('#nav-icon').toggleClass('open');
+	});
 });
 
 // Collapse navbar after onclick navbar-item
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
+    $('#nav-icon').toggleClass('open');
 });
-
-// Language selector
-function changeLanguage() {
-    var img1 = "icons/netherlands.svg";
-    var img2 = "icons/united-kingdom.svg";
-    
-    var imgElement = document.getElementById('language');
- 
-    imgElement.src = (imgElement.src == img1)? img2 : img1;
-}
+// }
