a5b790e87f933be317fbda2a55946d21c71f1c58
Sven Hansen <svenroermond@hotmail.com>
2021-01-11 01:32:54 +0100

    Added page content & responsive navbar including collapse transition
    Added page content, responsive navbar including collapse transition and footer content

--- script.js
+++ script.js
@@ -2,34 +2,42 @@ $(document).ready(function(){
 
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
-  
+
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
 
             // Prevent default anchor click behavior
             event.preventDefault();
-    
+                    
             // Store hash
             var hash = this.hash;
-    
-            // Using jQuery's animate() method to add smooth page scroll
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                       
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
+
+            // Let the navbar collapse in first (for 325ms) before scrolling to selected navbar-link
+            setTimeout(function() {
+                
+                // Using jQuery's animate() method to add smooth page scroll
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            }, 325)            
         }
     });
 });
 
-// // Language selector
-// function changeLanguage() {
-//     var img1 = "icons/netherlands.svg";
-//     var img2 = "icons/united-kingdom.svg";
+// Collapse navbar after onclick navbar-item
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+// Language selector
+function changeLanguage() {
+    var img1 = "icons/netherlands.svg";
+    var img2 = "icons/united-kingdom.svg";
     
-//     var imgElement = document.getElementById('language');
+    var imgElement = document.getElementById('language');
  
-//     imgElement.src = (imgElement.src == img1)? img2 : img1;
-// }
+    imgElement.src = (imgElement.src == img1)? img2 : img1;
+}
